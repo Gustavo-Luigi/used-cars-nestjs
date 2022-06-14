15416ac3b3658f9062097e7ba41438ed516b9802
Gustavo Luigi <gustavo_-_luigi@hotmail.com>
2022-06-13 23:06:44 -0300

    e2e test.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,6 +6,10 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { APP_PIPE } from '@nestjs/core';
+// Não é possivel importar cookieSession com import from devido algumas configurações do TS exigidas pelo NestJs
+// import {cookieSession} from 'cookie-session'
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
@@ -19,6 +23,25 @@ import { Report } from './reports/report.entity';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        // Whitelist true bloqueia propriedades indesejadas em requisições
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['asfsadfas'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// Não é possivel importar cookieSession com import from devido algumas configurações do TS exigidas pelo NestJs
-// import {cookieSession} from 'cookie-session'
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieSession({
-    keys: ['asfsadfas']
-  }))
-  app.useGlobalPipes(
-    new ValidationPipe({
-      // Whitelist true bloqueia propriedades indesejadas
-      whitelist: true,
-    }),
-  );
+  // app.use em main.ts funciona mas causa errors ao usar testes unitarios, a nao ser que inicializados por uma função, que poderia ser chamada no código do teste.
+  // app.use(cookieSession({
+  //   keys: ['asfsadfas']
+  // }))
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //   }),
+  // );
 
   await app.listen(3000);
 }
